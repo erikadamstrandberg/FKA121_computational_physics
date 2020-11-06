c85f03b793a88878ee47c242ff9a991bd3a5e0f7
Hanna Student <hallborn@student.chalmers.se>
2020-11-06 16:25:23 +0100

    fixed merge conflict

--- E1/printdisplacment.py
+++ E1/printdisplacment.py
@@ -20,9 +20,9 @@ array = np.genfromtxt('timetrail.csv', delimiter=',', skip_header=1)
 
 #%%
 fig, ax = plt.subplots()
-ax.plot(array[:, 5], array[:, 3])
-ax.plot(array[:, 5], array[:, 4])
-ax.plot(array[:, 5], array[:, 3]+array[:, 4])
+ax.plot(array[:, 5], array[:, 0])
+ax.plot(array[:, 5], array[:, 1])
+ax.plot(array[:, 5], array[:, 2])
 
 ax.set_xlabel('time (arb.unit)')
 ax.set_ylabel('signal (arb.unit)')
