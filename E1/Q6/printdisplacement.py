089a377494f51bf5ff6f7267af0d53ed4ce8230b
Erik Strandberg <erikadamstrandberg@gmail.com>
2020-11-09 15:49:35 +0100

    Added new dir for Q6

---
+++ E1/Q6/printdisplacement.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+###############################################################################
+# E1code2
+###############################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# skip_header skips the first
+# row in data.csv
+#%%
+array = np.genfromtxt('timetrail.csv', delimiter=',', skip_header=1)
+
+# q1 = array[:, 0]
+# q2 = array[:, 1]
+# q3 = array[:, 2]
+# U_kin = array[:, 3]
+# U_pot = array[:, 4]
+# time = array[:, 5]
+
+fig, ax = plt.subplots()
+ax.plot(array[:, 5], array[:, 3])
+ax.plot(array[:, 5], array[:, 4])
+ax.plot(array[:, 5], array[:, 3]+array[:, 4])
+
+ax.set_xlabel('time (arb.unit)')
+ax.set_ylabel('signal (arb.unit)')
+ax.grid()
+
+plt.show()
