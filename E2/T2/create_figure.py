4cf9c9dbc0b1b220088a0f851ba63cdaea4ad0a1
Erik Strandberg <erikadamstrandberg@gmail.com>
2020-11-14 18:32:59 +0100

    Created python code for plotting

--- E2/T2/create_figure.py
+++ E2/T2/create_figure.py
@@ -10,18 +10,38 @@ import matplotlib.pyplot as plt
 
 #%%
 
-array = np.genfromtxt('energy.csv', delimiter=',', skip_header=1)
-# U_kin = array[:, 0]
-# U_pot = array[:, 1]
-# time = array[:, 2]
+array = np.genfromtxt('energy_ord.csv', delimiter=',', skip_header=1)
+U_kin = array[:, 0]
+U_pot = array[:, 1]
+U_tot = array[:, 0] + array[:, 1]
+time  = array[:, 2]
 
 fig, ax = plt.subplots()
-ax.plot(array[:, 2], array[:, 0])
-ax.plot(array[:, 2], array[:, 1])
-ax.plot(array[:, 2], array[:, 0] + array[:,1])
+ax.plot(time, U_kin)
+ax.plot(time, U_pot)
+ax.plot(time, U_tot)
 
 ax.set_xlabel('time (arb.unit)')
 ax.set_ylabel('signal (arb.unit)')
 ax.grid()
 
+ax.set_xlim([0, 200])
 
+#%%
+
+U_tot_mean = sum(U_tot)/len(U_tot)
+print(U_tot_mean)
+
+#%%
+
+mode_energy = np.genfromtxt('energy_norm.csv', delimiter=',', skip_header=1)
+timesteps = len(mode_energy[0,:])
+
+fig, ax = plt.subplots()
+
+for i in range(timesteps):    
+    ax.plot(time, mode_energy[:, i])
+
+ax.set_xlabel('time (arb.unit)')
+ax.set_ylabel('signal (arb.unit)')  
+ax.grid()
