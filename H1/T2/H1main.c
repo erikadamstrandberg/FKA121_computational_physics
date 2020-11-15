2d61266497760a4f1455d07321afc4fe95d05038
Hanna Student <hallborn@student.chalmers.se>
2020-11-15 13:33:10 +0100

    verlet algorithm implemented

--- H1/T2/H1main.c
+++ H1/T2/H1main.c
@@ -15,9 +15,19 @@
 
 void random_displacement(double pos[][NDIM], int n_atoms, double interval);
 
+void verlet_timestep(double pos[][NDIM], double v[][NDIM], double f[][NDIM], 
+                     int n_atoms, 
+                     double dt, 
+                     double m, 
+                     double L);
+
 /* Main program */
-int main()
-{
+int main(){
+    double T = 0.001;
+    double dt = 0.0001;
+    int n_timesteps = T/dt;
+    printf("numper of timesteps: %d\n", n_timesteps);
+    double m_al = 13.0/9649.0;
     double a0 = 4.05;  
     int N = 4;
     int n_atoms = 4*N*N*N;
@@ -26,22 +36,13 @@ int main()
 
     init_fcc(pos, N, a0);
     random_displacement(pos, n_atoms, rand_interval);
-    /*
-    for(int i = 0; i < n_atoms; i++){
-        printf("%f\n", pos[i][0]);
-    }
-    */
 
-    /* 
-     Function that calculates the potential energy in units of [eV]. pos should be
-     a matrix containing the positions of all the atoms, L is the length of the 
-     supercell and N is the number of atoms.
-    */
-     double L = N*a0;
-     double energy;
-     energy = get_energy_AL(pos, L, n_atoms);
+    double L = N*a0;
+    double energy;
+    energy = get_energy_AL(pos, L, n_atoms);
     
-     printf("%f\n", energy);
+    printf("potential energy: %f\n", energy);
+
     /* 
      Function that calculates the virial in units of [eV]. pos should be a matrix
      containing the positions of all the atoms, L is the length of the supercell 
@@ -54,16 +55,30 @@ int main()
     
     /*
      Function that calculates the forces on all atoms in units of [eV/Å]. the 
-     forces are stored in f which should be a matrix of size N x 3, where N is the
-     number of atoms and column 1,2 and 3 correspond to the x,y and z component of
+     forces are stored in f which should be a matrix of size n_atoms x 3, where
+     column 1,2 and 3 correspond to the x,y and z component of
      the force resepctively . pos should be a matrix containing the positions of 
      all the atoms, L is the length of the supercell and N is the number of atoms.
     */
-    /*
-     get_forces_AL(f,pos, L, N);
-    */
+    double f[n_atoms][NDIM];
+    double v[n_atoms][NDIM]; 
+    for(int i = 0; i < n_atoms; i++){
+        for(int j = 0; j < NDIM; j++){
+            f[i][j] = 0.0;
+            v[i][j] = 0.0;
+        }
+    }
+
+    get_forces_AL(f,pos, L, n_atoms);
     
-    
+    for(int t = 0; t < n_timesteps; t++){
+
+        verlet_timestep(pos, v, f, n_atoms, dt, m_al, L);
+       
+        energy = get_energy_AL(pos, L, n_atoms);
+        printf("timestep: %d\t potential energy: %f\n", t, energy);
+    }
+
     
 }
 
@@ -77,6 +92,28 @@ void random_displacement(double pos[][NDIM], int n_atoms, double interval){
             random_value = (double) rand() / (double) RAND_MAX;
             random_disp = random_value*2*interval-interval;
             pos[i][j] += random_disp;
+        }
+    }
+}
+
+void verlet_timestep(double pos[][NDIM], double v[][NDIM], double f[][NDIM], 
+                     int n_atoms, 
+                     double dt, 
+                     double m, 
+                     double L){
+
+    for(int i = 0; i < n_atoms; i++){
+        for(int j = 0; j < NDIM; j++){
+            v[i][j] += dt*f[i][j]/(2.0*m);
+            pos[i][j] += dt*v[i][j];
+        }
+    }
+
+    get_forces_AL(f,pos, L, n_atoms);
+
+    for(int i = 0; i < n_atoms; i++){
+        for(int j = 0; j < NDIM; j++){
+            v[i][j] += dt*f[i][j]/(2.0*m);
         }
     }
 }
