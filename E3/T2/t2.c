77a50604471b3791da9974b2105b9dce811df123
Hanna Student <hallborn@student.chalmers.se>
2020-11-23 22:40:11 +0100

    E3 T2 integration works!

--- E3/T2/t2.c
+++ E3/T2/t2.c
@@ -4,8 +4,31 @@
 #include <math.h>
 #include <time.h>
 
+#define PI 3.14159265359
 
 // Main 
 int main(){
-    printf("hejhejhej");
+    int N = 10000;
+    double x = 0.0;
+    double y = 0.0;
+    double gi = 0.0;
+    double gi2 = 0.0;
+    double I = 0.0;
+    double sigma2 = 0.0;
+
+    srand(time(NULL));
+
+    for(int i = 0; i < N; i++){ 
+        x = (double) rand()/(double) RAND_MAX;
+        y = (1.0/PI)*acos(1.0-2.0*x);
+        
+        gi += (2.0/PI)*(y*(1-y))/(sin(PI*y));
+        gi2 += pow((2.0/PI)*(y*(1-y))/(sin(PI*y)),2);
+    }
+
+    I = gi/N;
+    sigma2 = gi2/N-pow(gi/N,2);
+
+    printf("%f\n", I);
+    printf("%f\n", sigma2);
 }
