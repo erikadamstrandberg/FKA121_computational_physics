9236fb32ba4e9d2c407859b8e35875f7772b21e3
erikadamstrandberg <erikadamstrandberg@gmail.com>
2020-11-24 00:13:01 +0100

    Plotting weight and integral in E3/T2

--- E3/T2/figure.py
+++ E3/T2/figure.py
@@ -4,6 +4,80 @@ import matplotlib.pyplot as plt
 
 #%%
 
-array = np.genfromtxt('dist.csv', delimiter=',', skip_header=1)
+array = np.genfromtxt('dist_10.csv', delimiter=',', skip_header=1)
 
-uniform = array[1,:]
+uniform = array[:,0]
+sinx_dist = array[:,1]
+
+b = np.arange(0,1.1,0.05)
+
+fig, ax = plt.subplots()
+plt.hist(sinx_dist, bins=b) 
+
+plt.savefig('sinx_N_10.pdf', format='pdf', bbox_inches='tight')
+plt.show()
+
+#%%
+
+array = np.genfromtxt('dist_100.csv', delimiter=',', skip_header=1)
+
+uniform = array[:,0]
+sinx_dist = array[:,1]
+
+b = np.arange(0,1.1,0.05)
+
+fig, ax = plt.subplots()
+plt.hist(sinx_dist, bins=b) 
+
+plt.savefig('sinx_N_100.pdf', format='pdf', bbox_inches='tight')
+plt.show()
+
+#%%
+
+array = np.genfromtxt('dist_1000.csv', delimiter=',', skip_header=1)
+
+uniform = array[:,0]
+sinx_dist = array[:,1]
+
+b = np.arange(0,1.1,0.02)
+
+fig, ax = plt.subplots()
+plt.hist(sinx_dist, bins=b) 
+
+plt.savefig('sinx_N_1000.pdf', format='pdf', bbox_inches='tight')
+plt.show()
+
+#%%
+
+array = np.genfromtxt('dist_10000.csv', delimiter=',', skip_header=1)
+
+uniform = array[:,0]
+sinx_dist = array[:,1]
+
+b = np.arange(0,1.1,0.02)
+
+fig, ax = plt.subplots()
+plt.hist(sinx_dist, bins=b) 
+
+plt.savefig('sinx_N_10000.pdf', format='pdf', bbox_inches='tight')
+plt.show()
+
+
+#%%
+
+array = np.genfromtxt('dist_100000.csv', delimiter=',', skip_header=1)
+
+uniform = array[:,0]
+sinx_dist = array[:,1]
+
+b = np.arange(0,1.1,0.001)
+
+fig, ax = plt.subplots()
+plt.hist(sinx_dist, bins=b) 
+
+plt.savefig('sinx_N_10000.pdf', format='pdf', bbox_inches='tight')
+plt.show()
+
+#%%
+
+
