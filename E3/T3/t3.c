3f56975d1bfa5bdb8dd51f650bc7bcc9df35ec42
Hanna Student <hallborn@student.chalmers.se>
2020-11-25 23:43:08 +0100

    fixed r --> r_x och N_burn

--- E3/T3/t3.c
+++ E3/T3/t3.c
@@ -18,8 +18,8 @@ double integrand(double *x, double *y, double *z){
 
 // Main 
 int main(){
-    int N = 10000;
-
+    int N = 100000;
+    int N_burn = 1000;
     double x[N];
     double y[N];
     double z[N];
@@ -29,8 +29,10 @@ int main(){
     double x_trial;
     double y_trial;
     double z_trial;
-    double r;
-    double delta = 2.5;
+    double r_x;
+    double r_y;
+    double r_z;
+    double delta = 2.0;
 
     srand(time(NULL));
     x[0] = (double) rand()/(double) RAND_MAX;
@@ -39,15 +41,17 @@ int main(){
 
     int accept = 0;
     for(int i = 0; i < N-1; i++){
-        r = (double) rand()/(double) RAND_MAX;
-        x_trial = x[i] + delta*(r-0.5);
-        y_trial = y[i] + delta*(r-0.5);
-        z_trial = z[i] + delta*(r-0.5);
+        r_x = (double) rand()/(double) RAND_MAX;
+        r_y = (double) rand()/(double) RAND_MAX;
+        r_z = (double) rand()/(double) RAND_MAX;
+        x_trial = x[i] + delta*(r_x-0.5);
+        y_trial = y[i] + delta*(r_y-0.5);
+        z_trial = z[i] + delta*(r_z-0.5);
 
         f = weight(&x[i], &y[i], &z[i]);
         f_trial = weight(&x_trial, &y_trial, &z_trial);
 
-        if(f_trial > f || f_trial/f > r){
+        if(f_trial > f || f_trial/f > r_x){
             x[i+1] = x_trial;    
             y[i+1] = y_trial;    
             z[i+1] = z_trial;
@@ -63,12 +67,12 @@ int main(){
 
 
     double integral = 0.0;
-    for(int i = 0; i < N; i++){
+    for(int i = N_burn; i < N; i++){
         integral += integrand(&x[i], &y[i], &z[i]); 
     }
 
 
-    integral = integral/N;
+    integral = integral/(N-N_burn);
     printf("%f\n", integral);
     printf("%f\n", (double) accept/(double) N);
 }
