0644c41c277b521052b8a56bfe12362b2ac9e69b
erikadamstrandberg <erikadamstrandberg@gmail.com>
2020-11-26 00:36:07 +0100

    New acceptance ratio. Calculating the ratio for the duel stage!

--- E3/T3/t3.c
+++ E3/T3/t3.c
@@ -9,7 +9,7 @@
 
 
 double weight(double *x, double *y, double *z){
-    return pow(PI, -3.0/2.0)*exp(-(pow(*x,2)+pow(*y,2)+pow(*z,2)));
+    return pow(PI, -3.0/2.0)*exp(-(pow(*x,2) + pow(*y,2) + pow(*z,2)));
 }
 
 double integrand(double *x, double *y, double *z){
@@ -18,8 +18,9 @@ double integrand(double *x, double *y, double *z){
 
 // Main 
 int main(){
-    int N = 100000;
-    int N_burn = 1000;
+    int N = 300000;
+    int N_burn = 20000;
+
     double x[N];
     double y[N];
     double z[N];
@@ -32,7 +33,9 @@ int main(){
     double r_x;
     double r_y;
     double r_z;
-    double delta = 2.0;
+    double delta = 2.4;
+
+    double accept_rand;
 
     srand(time(NULL));
     x[0] = (double) rand()/(double) RAND_MAX;
@@ -40,10 +43,12 @@ int main(){
     z[0] = (double) rand()/(double) RAND_MAX;
 
     int accept = 0;
+    int non_accept = 0;
     for(int i = 0; i < N-1; i++){
         r_x = (double) rand()/(double) RAND_MAX;
         r_y = (double) rand()/(double) RAND_MAX;
         r_z = (double) rand()/(double) RAND_MAX;
+
         x_trial = x[i] + delta*(r_x-0.5);
         y_trial = y[i] + delta*(r_y-0.5);
         z_trial = z[i] + delta*(r_z-0.5);
@@ -51,20 +56,34 @@ int main(){
         f = weight(&x[i], &y[i], &z[i]);
         f_trial = weight(&x_trial, &y_trial, &z_trial);
 
-        if(f_trial > f || f_trial/f > r_x){
+        accept_rand = (double) rand()/(double) RAND_MAX;
+        
+        if(f_trial > f){
             x[i+1] = x_trial;    
             y[i+1] = y_trial;    
             z[i+1] = z_trial;
-
-            accept += 1;   
-
-        }else{
-            x[i+1] = x[i];    
-            y[i+1] = y[i];    
-            z[i+1] = z[i];    
+        } else {
+            if (f_trial/f > accept_rand){
+                x[i+1] = x_trial;    
+                y[i+1] = y_trial;    
+                z[i+1] = z_trial;
+                accept += 1;
+            } else {
+                x[i+1] = x[i];    
+                y[i+1] = y[i];    
+                z[i+1] = z[i];
+                non_accept += 1;
+            }
         }
     }
 
+
+    FILE *fp = fopen("walkers.csv", "w");
+    fprintf(fp, "x,y,z\n");
+    for(int i = 0; i < N; i++){
+        fprintf(fp, "%f,%f,%f\n", x[i], y[i], z[i]);
+    }
+    fclose(fp);
 
     double integral = 0.0;
     for(int i = N_burn; i < N; i++){
@@ -73,6 +92,8 @@ int main(){
 
 
     integral = integral/(N-N_burn);
-    printf("%f\n", integral);
-    printf("%f\n", (double) accept/(double) N);
+
+    printf("Exact integral: %f\n", 7.0/8.0);
+    printf("Calculated integral: %f\n", integral);
+    printf("Acceptance ratio: %f\n\n", (double) accept/(double) non_accept);
 }
