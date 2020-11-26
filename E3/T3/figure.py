2567c5236e32d9d59a60320e72b05736d074a650
erikadamstrandberg <erikadamstrandberg@gmail.com>
2020-11-26 11:52:09 +0100

    Started with T4

--- E3/T3/figure.py
+++ E3/T3/figure.py
@@ -4,11 +4,15 @@ import matplotlib.pyplot as plt
 
 #%%
 
-array = np.genfromtxt('walkers.csv', delimiter=',', skip_header=1)
+array = np.genfromtxt('N.csv', delimiter=',', skip_header=1)
+N = array[0]
+
+array = np.genfromtxt('walkers.csv', delimiter=',', skip_header=3)
 
 x = array[:,0]
 y = array[:,1]
 z = array[:,2]
+dist = array[:,3]
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -21,10 +25,14 @@ ax.plot(x)
 
 #%%
 
-step_bin = 0.05
+step_bin = 0.001
 start_bin = 0
-stopp_bin = 1 + step_bin
+stopp_bin = 0.2 + step_bin
 b = np.arange(start_bin, stopp_bin, step_bin)
 
 fig, ax = plt.subplots()
-plt.hist(sinx_dist, bins=b) 
+plt.hist(dist, bins=b, density=True) 
+
+
+#%%
+
---
+++ E3/T4/main.py
@@ -0,0 +1,8 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+
+array = np.genfromtxt('MC.txt', delimiter=',', skip_header=1)
+
