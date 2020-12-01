08674f2b770546d9af4ca5bede666a19fb725b4a
Hanna Student <hallborn@student.chalmers.se>
2020-12-01 14:22:41 +0100

    added merge of TPV in merge_trails.py

--- H1/T3/merge_trails.py
+++ H1/T3/merge_trails.py
@@ -6,6 +6,7 @@ def main():
 
     pos_data = np.genfromtxt('../T3/q_trail_0.csv', delimiter=',')
     vel_data = np.genfromtxt('../T3/v_trail_0.csv', delimiter=',')
+    TPV = np.genfromtxt('../T3/TPV_0.csv', delimiter=',', skip_header=1)
 
     for i in range(nfiles-1):
         filenr = i+1
@@ -16,9 +17,13 @@ def main():
         vel_data_temp = np.genfromtxt('../T3/v_trail_'+ str(filenr) + '.csv', delimiter=',')
         vel_data = np.append(vel_data, vel_data_temp, axis=0)
 
+        TPV_temp = np.genfromtxt('../T3/TPV_'+ str(filenr) + '.csv', delimiter=',', skip_header=1)
+        TPV = np.append(TPV, TPV_temp, axis=0)
+
 
 
     np.savetxt('merged_q_trail.csv', pos_data, delimiter=',') 
     np.savetxt('merged_v_trail.csv', vel_data, delimiter=',')
+    np.savetxt('merged_TPV.csv', TPV, delimiter=',')
 
 main()
