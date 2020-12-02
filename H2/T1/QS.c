6f429151e952103b4e6f4e77fb10f89d47934a5e
erikadamstrandberg <erikadamstrandberg@gmail.com>
2020-12-02 23:32:53 +0100

    Set up the structure for H2b, variational Monte Carlo

---
+++ H2/T1/QS.c
@@ -0,0 +1,11 @@
+// First task for H2b
+// Benchmarking for the Monte Carlo algorithm
+
+// C includes
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    printf("hej");
+}
