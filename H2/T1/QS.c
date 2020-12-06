4d8128d2c3ee2e55e5c0b39a4c644cf45379d123
Hanna Student <hallborn@student.chalmers.se>
2020-12-06 22:28:05 +0100

    added  print_to_file.c

--- H2/T1/QS.c
+++ H2/T1/QS.c
@@ -15,23 +15,7 @@
 // Includes from objective files
 #include "init_gsl.h"
 #include "local_energy.h"
-
-void print_markov(double *E_l, int N,
-                  double *x1, double *y1, double *z1, double *x2, double *y2, double *z2,
-                  char*filename)
-{
-    char *filename_csv = malloc((strlen(filename) + 4)*sizeof(char));
-
-    strcpy(filename_csv, filename);
-    strcat(filename_csv, ".csv");
-
-    FILE *mc = fopen(filename_csv, "w");
-    for(int i = 0; i < N; i++)
-    {
-       fprintf(mc, "%f,%f,%f,%f,%f,%f,%f\n", x1[i], y1[i], z1[i], x2[i], y2[i], z2[i], E_l[i]);
-    }
-    fclose(mc);
-}
+#include "print_to_file.h"
 
 // Main 
 int main()
