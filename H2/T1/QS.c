01adc4e55924e7fc896bc684ae05fbdc65b34284
erikadamstrandberg <erikadamstrandberg@gmail.com>
2020-12-06 21:56:58 +0100

    Refactor of local energy into modifing the entire array E_l

--- H2/T1/QS.c
+++ H2/T1/QS.c
@@ -38,25 +38,29 @@ void weight(double *w, double *alpha,
 
 }
 
-void local_energy(double *E_l, double *alpha, 
+void local_energy(double *E_l, double *alpha, int N, 
                   double *x1, double *y1, double *z1, double *x2, double *y2, double *z2)
 {
     double *r1 = malloc(sizeof(double));
     double *r2 = malloc(sizeof(double));
     double *r12 = malloc(sizeof(double));
     double *snd_term = malloc(sizeof(double));
-    
-    *r1  = sqrt(pow(*x1,2)       + pow(*y1,2)       + pow(*z1,2));
-    *r2  = sqrt(pow(*x2,2)       + pow(*y2,2)       + pow(*z2,2));
-    *r12 = sqrt(pow((*x2-*x1),2) + pow((*y2-*y1),2) + pow((*z2-*z1),2));
-    
-    *snd_term = (((*r2)*(*x1)-(*r1)*(*x2))*((*x1)-(*x2))+
-                 ((*r2)*(*y1)-(*r1)*(*y2))*((*y1)-(*y2))+
-                 ((*r2)*(*z1)-(*r1)*(*z2))*((*z1)-(*z2)))
-                /((*r1)*(*r2)*(*r12)*pow((1+(*alpha)*(*r12)),2));
 
-    *E_l = -4.0 + (*snd_term) -1.0/((*r12)*pow((1.0 + (*alpha)*(*r12)),3)) 
+    for (int i = 0; i < N; i++)
+    {
+        *r1  = sqrt(pow(x1[i],2)       + pow(y1[i],2)       + pow(z1[i],2));
+        *r2  = sqrt(pow(x2[i],2)      + pow(y2[i],2)     + pow(z2[i],2));
+        *r12 = sqrt(pow((x2[i]-x1[i]),2) + pow((y2[i]-y1[i]),2) + pow((z2[i]-z1[i]),2));
+
+        *snd_term = (((*r2)*x1[i]-(*r1)*x2[i])*(x1[i]-x2[i])+
+                     ((*r2)*y1[i]-(*r1)*y2[i])*(y1[i]-y2[i])+
+                     ((*r2)*z1[i]-(*r1)*z2[i])*(z1[i]-z2[i]))
+                    /((*r1)*(*r2)*(*r12)*pow((1+(*alpha)*(*r12)),2));
+
+        E_l[i] = -4.0 + (*snd_term) -1.0/((*r12)*pow((1.0 + (*alpha)*(*r12)),3)) 
            -1.0/(4.0*pow((1.0 + (*alpha)*(*r12)),4)) + 1.0/(*r12); 
+
+    }
 
     free(r1);
     free(r2);
@@ -79,7 +83,7 @@ int main()
     rand = gsl_rng_uniform(gsl_rand);
 
     double alpha = 0.1;
-    int N = 10000;
+    int N = 20000;
     double x1[N];
     double y1[N];
     double z1[N];
@@ -142,17 +146,16 @@ int main()
            z2[i+1] = z2[i];
         }
     }
+
     printf("%f\n", (double) accept/((double) N));
     
-    for(int i = 0; i < N; i++)
-    {
-        local_energy(&E_l[i], &alpha, &x1[i], &y1[i], &z1[i], &x2[i], &y2[i], &z2[i]);
-    }
+    local_energy(E_l, &alpha, N, x1, y1, z1, x2, y2, z2);
+   
 
     FILE *mc = fopen("markov_chain.csv", "w");
     for(int i = 0; i < N; i++)
     {
-        fprintf(mc, "%f,%f,%f,%f,%f,%f,%f\n", x1[i], y1[i], z1[i], x2[i], y2[i], z2[i], E_l[i]);
+       fprintf(mc, "%f,%f,%f,%f,%f,%f,%f\n", x1[i], y1[i], z1[i], x2[i], y2[i], z2[i], E_l[i]);
     }
     fclose(mc);
 }
