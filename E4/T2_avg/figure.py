f54cbc2575cbbb57373700392014fb77d0019de9
erikadamstrandberg <erikadamstrandberg@gmail.com>
2020-12-10 22:20:32 +0100

    Added analytical damped pendulum

--- E4/T2_avg/figure.py
+++ E4/T2_avg/figure.py
@@ -3,17 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #%%
-array = np.genfromtxt('../T2_avg/spectrum.csv', delimiter=',')
-vfft = array[:,0]
-freq = array[:,1]  
 
-fig, ax = plt.subplots()
-ax.plot(freq, vfft)
+kb = 1.38e-23
+T = 297
+rho = 2650
+d = 2.79e-6
+r = d/2
+m_pearl = rho*4*np.pi*r**3/3
 
-ax.set_xlim([0,10])
+tau_low = 147.3*1e-3
+tau_high = 48.5*1e-3
+
+mu_low = 1/tau_low
+mu_high = 1/tau_high
+
+f = np.linspace(-20,20,1000)
+anal_low = (kb*T/m_pearl)*(2*mu_low/((2*np.pi*f)**2+mu_low**2))
+anal_high = (kb*T/m_pearl)*(2*mu_high/((2*np.pi*f)**2+mu_high**2))
 
 
-#%%
 array = np.genfromtxt('../T2_avg/data/spectrum_high.csv', delimiter=',')
 vfft_high = array[:,0]
 freq = array[:,1]  
@@ -24,12 +32,15 @@ vfft_low = array[:,0]
 fig, ax = plt.subplots()
 ax.plot(freq, vfft_high, label=r'$P_{high} = 99.8kPa$')
 ax.plot(freq, vfft_low,  label=r'$P_{low} = 2.75kPa$')
+ax.plot(f+3.1, anal_low*1.1e4,  '--', label=r'$P_{a,low}$')
+ax.plot(f+3.1, anal_high*1.1e4, '--', label=r'$P_{a,high}$')
 
 ax.set_xlim([0,10])
 
 ax.set_title(r'Averaged spectrum from v-trail, $d\tau = 0.05\ ms$', fontsize='16')
 ax.set_xlabel(r'$f$ [$kHz$]', fontsize='16')
 ax.set_ylabel(r'$P$ [$arb.u.$]', fontsize='16')
+
 
 ax.grid()
 ax.legend(fontsize='16', loc='upper right')
