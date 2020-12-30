648d9b9ec70e9c3dfd6e20d56da541d4dc11dd7c
erikadamstrandberg <erikadamstrandberg@gmail.com>
2020-12-31 00:21:07 +0100

    ASU_prim done and T1 started

--- H3/T1/QS.c
+++ H3/T1/QS.c
@@ -1,15 +1,58 @@
-// H2b Variational Monto Carlo
+// H3b Time independent quantum mechanics 
 // 
-// T1 benchmarking
+// T1 making the FFT work!
 
 // Standard C libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <complex.h>
+#include <fftw3.h>
+
+// Our own includes
+#include "fft.h"
+#include "write_to_file.h"
+
+// Constants
+#define PI 3.14159
 
 // Main 
 int main()
 {
-    printf("hej");
+    int N = 1000;
+    double x[N];
+    complex x_complex[N];
+    double t[N];
+
+    double f  = 1;
+    double dt = 0.01;
+    double a  = 1;
+
+    for(int i = 0; i < N; i++)
+    {
+        x[i] = a*cos(2*PI*f*i*dt);
+        x_complex[i] = a*cos(2*PI*f*i*dt);
+        t[i] = dt*i;
+    }
+
+    print_1d_array(x, N, "signal");
+    print_1d_array(t, N, "time");
+
+    double spectrum[N];
+    double freq[N];
+
+    fftw_complex out[N];
+    fftw_plan p;
+    p = fftw_plan_dft_1d(N, x_complex, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    fftw_execute(p);
+
+    complex out_complex[N];
+    for(int i = 0; i < N; i++)
+    {   
+        out_complex[i] = creal(out[i]) + I*cimag(out[i]);
+    }
+    
+    print_complex_array(out_complex, N, "out");
 }
--- H3/T1/figure.py
+++ H3/T1/figure.py
@@ -1,253 +1,135 @@
-#%% T! Benchmarking for variational Monte Carlo
-# 
+#%% H3b time-independent scroooodinger
+
 # Standard includes
 import numpy as np
 import matplotlib.pyplot as plt
 
+#%% Rescaling into ASU_prim units for H3b
+
+u = 1.66054e-27    ## Atmoic weight in kg
+
+T_asup = 1e-15      ## fs 
+L_asup = 1e-10      ## angstrom
+E_asup = 1.602e-19  ## eV
+
+m_asup = E_asup*T_asup**2/L_asup**2 # Mass unit in ASU_prim
+m_asup_u = m_asup/u                 # Mass unti in ASU_prim per u
+
+hbar_si = 1.054571817e-34           # hbar in SI
+hbar_asup = hbar_si*1e34/1.602      # hbar in ASU_prim. eV*fs
+
+print(f"Mass in asup: {m_asup_u} u")
+print(f"h_bar in asup: {hbar_asup} eV*fs")
+
+#%% Plotting the Gaussian wave packet
+
+def wave_package_position(x, x0, p0, d):
+    hbar_si = 1.054571817e-34
+    hbar_asup = hbar_si*1e34/1.602
+    
+    prefactor = (1/(np.pi*d**2))**(1/4)
+    envelope  = np.exp(-(x - x0)**2/(2*d**2))
+    plane_wave = np.exp(1j*p0*(x - x0)/hbar_asup)
+    return prefactor*envelope*plane_wave
+
+d   = 0.5               # Width of our hydrogen atom
+m_h = 1*m_asup_u        # Mass of our hydrogen atom
+
+p0 = np.sqrt(0.2*m_h)   # Initial momentum of our hydrogen atom
+x0 = 0.4                # Initial position of our hydrogen atom
+
+dx      = 0.01
+x_start = -100
+x_stop  = 100
+x = np.arange(x_start, x_stop, dx)
+Nx = len(x)
+
+phi_x = wave_package_position(x, x0, p0, d)
+n_x = np.abs(phi_x)**2
+
+fig, ax = plt.subplots()
+ax.plot(x, n_x)
+
+#%%
+
+p = np.fft.fftshift(np.fft.fftfreq(Nx, d=dx))
+
+phi_p = np.fft.fftshift(np.fft.fft(phi_x))
+n_p = np.abs(phi_p)**2
+ 
+
+x = np.array([0,0])
+y = np.array([0,17500])
+fig, ax = plt.subplots()
+ax.plot(p, n_p)
+ax.plot(x,y)
+
+ax.set_xlim([-1,1])
+
+
+#%%
+
+def create_V11(x, a, b):
+    V11 = np.zeros(len(x))
+    for i in range(len(x)):
+        if (x[i] < 0):
+            V11[i] = a*np.exp(x[i]/b)
+        else:
+            V11[i] = a*(2 - np.exp(-x[i]/b))
+            
+    return V11
+
+def create_V22(x, a, b):
+    return 2*a - create_V11(x, a, b)
+
+def create_V12(x, c, d):
+    return c*np.exp(-(x/d)**2)
+ 
+
+
+x = np.linspace(-4,4,100)
+a = 1
+b = 1
+c = 2
+d = 1
+
+V11 = create_V11(x, a, b)
+V22 = create_V22(x, a, b)
+V12 = create_V12(x, c, d)
+
+fig, ax = plt.subplots()
+ax.plot(x, V11)
+ax.plot(x, V22)
+ax.plot(x, V12)
+
+
+#fig, ax = plt.subplots()
+#ax.plot(x, np.real(phi_x))
+#ax.plot(x, np.imag(phi_x)/np.max(np.imag(phi_x)))
+
+
+
 
 #%% Uncorrelated! With only one electrion wavefunctions
-array = np.genfromtxt('../T1/data/markov_chain_uncorrelated.csv', delimiter=',')
-
-## Markox chain with coordinates for first electron
-x1 = array[:,0]
-y1 = array[:,1]
-z1 = array[:,2]
-
-## Markox chain with coordinates for second electron    
-x2 = array[:,3]
-y2 = array[:,4]
-z2 = array[:,5]
-
-r1 = np.zeros(len(array[:,0]))
-r2 = np.zeros(len(array[:,0]))
-theta = np.zeros(len(array[:,0]))
-uniform = np.zeros(len(array[:,0]))
-
-for i in range(len(r1)):
-    r1[i] = np.sqrt(np.dot(x1[i],x1[i]) + np.dot(y1[i],y1[i]) + np.dot(z1[i],z1[i]))
-    r2[i] = np.sqrt(np.dot(x2[i],x2[i]) + np.dot(y2[i],y2[i]) + np.dot(z2[i],z2[i]))
-    
-    arg = (x1[i]*x2[i] + y1[i]*y2[i] + z1[i]*z2[i])
-    theta[i] = np.arccos(arg/(r1[i]*r2[i]))
-    uniform[i] = np.cos(theta[i])
-
-
-#%% Bins for histogram
-step_bin = 0.05
-start_bin = -1
-stopp_bin = 1 + step_bin
-b = np.arange(start_bin, stopp_bin, step_bin)
-
-x = np.array([-1,1])
-y = np.array([1/2,1/2])
+signal = np.genfromtxt('../T1/signal.csv', delimiter=',')
+time   = np.genfromtxt('../T1/time.csv', delimiter=',')
+ 
 
 fig, ax = plt.subplots()
-ax.hist(uniform, bins=b, density=True, label=r'$x=\cos(\theta)$')
-ax.plot(x,y, '--', color='black', label=r'$1/2$', linewidth=2)
+ax.plot(time, signal)
 
-ax.set_title(r'Distribution of $x=\cos(\theta)$ for $\Psi_{un}$', fontsize='16')
-ax.set_xlabel(r'$x$', fontsize='16')
-ax.set_ylabel(r'$\rho$', fontsize='16')
-
-ax.set_ylim([0,0.8])
-
-ax.grid()
-ax.legend(fontsize='16', loc='upper right')
-
-plt.savefig('../T1/figure/uniform_uncorrelated.pdf', format='pdf', bbox_inches='tight')
-plt.show()
-
-#%% Bins for histogram
-step_bin = 0.05
-start_bin = 0
-stopp_bin = np.pi + step_bin
-b = np.arange(start_bin, stopp_bin, step_bin)
-
-x = np.linspace(0,np.pi,500)
-y = (1/2)*np.sin(x)
-
-
-fig, ax = plt.subplots()
-ax.hist(theta, bins=b, density=True, label=r'$\theta$')
-ax.plot(x,y, '--', color='black', label=r'$1/2\sin(\theta)$', linewidth=2)
-
-ax.set_title(r'Distribution of $\theta$ for $\Psi_{un}$', fontsize='16')
-ax.set_xlabel(r'$\theta$', fontsize='16')
-ax.set_ylabel(r'$\rho$', fontsize='16')
-
-ax.grid()
-ax.legend(fontsize='16', loc='upper left')
-
-plt.savefig('../T1/figure/theta_uncorrelated.pdf', format='pdf', bbox_inches='tight')
-plt.show()
-
-
-#%% Correlated data
-array = np.genfromtxt('../T1/data/markov_chain_correlated.csv', delimiter=',')
-
-## Markox chain with coordinates for first electron
-x1 = array[:,0]
-y1 = array[:,1]
-z1 = array[:,2]
-
-## Markox chain with coordinates for second electron    
-x2 = array[:,3]
-y2 = array[:,4]
-z2 = array[:,5]
-
-El = array[:,6]
-
-N = len(array[:,0])
-
-
-r1 = np.zeros(len(array[:,0]))
-r2 = np.zeros(len(array[:,0]))
-theta = np.zeros(len(array[:,0]))
-uniform = np.zeros(len(array[:,0]))
-
-for i in range(len(r1)):
-    r1[i] = np.sqrt(np.dot(x1[i],x1[i]) + np.dot(y1[i],y1[i]) + np.dot(z1[i],z1[i]))
-    r2[i] = np.sqrt(np.dot(x2[i],x2[i]) + np.dot(y2[i],y2[i]) + np.dot(z2[i],z2[i]))
-    
-    arg = (x1[i]*x2[i] + y1[i]*y2[i] + z1[i]*z2[i])
-    theta[i] = np.arccos(arg/(r1[i]*r2[i]))
-    uniform[i] = np.cos(theta[i])
-
-
-#%% Bins for histogram
-step_bin = 0.05
-start_bin = -1
-stopp_bin = 1 + step_bin
-b = np.arange(start_bin, stopp_bin, step_bin)
-
-x = np.array([-1,1])
-y = np.array([1/2,1/2])
-
-fig, ax = plt.subplots()
-ax.hist(uniform, bins=b, density=True, label=r'$x=\cos(\theta)$')
-ax.plot(x,y, '--', color='black', label=r'$1/2$', linewidth=2)
-
-ax.set_title(r'Distribution of $x=\cos(\theta)$', fontsize='16')
-ax.set_xlabel(r'$x$', fontsize='16')
-ax.set_ylabel(r'$\rho$', fontsize='16')
-
-ax.grid()
-ax.legend(fontsize='16', loc='upper right')
-
-plt.savefig('../T1/figure/uniform_correlated.pdf', format='pdf', bbox_inches='tight')
-plt.show()
-
-#%% Bins for histogram
-step_bin = 0.05
-start_bin = 0
-stopp_bin = np.pi + step_bin
-b = np.arange(start_bin, stopp_bin, step_bin)
-
-x = np.linspace(0,np.pi,500)
-y = (1/2)*np.sin(x)
-
-
-fig, ax = plt.subplots()
-ax.hist(theta, bins=b, density=True, label=r'$\theta$')
-ax.plot(x,y, '--', color='black', label=r'$1/2\sin(\theta)$', linewidth=2)
-
-ax.set_title(r'Distribution of $\theta$', fontsize='16')
-ax.set_xlabel(r'$\theta$', fontsize='16')
-ax.set_ylabel(r'$\rho$', fontsize='16')
-
-ax.grid()
-ax.legend(fontsize='16', loc='upper left')
-
-plt.savefig('../T1/figure/theta_correlated.pdf', format='pdf', bbox_inches='tight')
-plt.show()
-
-
-#%% Centeral mean field approx. from Hartree
-Z_unscreened = 2
-Z_optimized  = 27/16
-
-r = np.linspace(0, 5, 1000)
-def rho(r, Z):
-    return Z**3*4*r**2*np.exp(-2*Z*r)
-
-## Bins for histogram
-step_bin = 0.05
-start_bin = 0
-stopp_bin = 5 + step_bin
-b = np.arange(start_bin, stopp_bin, step_bin)
-
-# NORMALIZE!?!
-rho_unscreened = rho(r, Z_unscreened)
-rho_optimized = rho(r, Z_optimized)
-fig, ax = plt.subplots()
-ax.hist(r1, bins=b, density=True, label=r'$\rho(r_1)$')
-ax.plot(r, rho_unscreened, '--', color='black', label=r'$\rho_{unscreened}(r)$', linewidth=2)
-ax.plot(r, rho_optimized , '--', color='red', label=r'$\rho_{screened}(r)$', linewidth=2)
-
-ax.set_title(r'Benchmarking $\rho(r_1)$ sampling, $\alpha=0.1$', fontsize='16')
-ax.set_xlabel(r'$r$ [$a_0$]', fontsize='16')
-ax.set_ylabel(r'$\rho$', fontsize='16')
-
-ax.grid()
-ax.legend(fontsize='16', loc='upper right')
-
-plt.savefig('../T1/figure/probability_benchmark_r1.pdf', format='pdf', bbox_inches='tight')
-plt.show()
-
-fig, ax = plt.subplots()
-ax.hist(r2, bins=b, density=True, label=r'$\rho(r_2)$')
-ax.plot(r, rho_unscreened, '--', color='black', label=r'$\rho_{unscreened}(r)$', linewidth=2)
-ax.plot(r, rho_optimized , '--', color='red', label=r'$\rho_{screened}(r)$', linewidth=2)
-
-ax.set_title(r'Benchmarking $\rho(r_2)$ sampling, $\alpha=0.1$', fontsize='16')
-ax.set_xlabel(r'$r$ [$a_0$]', fontsize='16')
-ax.set_ylabel(r'$\rho$', fontsize='16')
-
-ax.grid()
-ax.legend(fontsize='16', loc='upper right')
-
-plt.savefig('../T1/figure/probability_benchmark_r2.pdf', format='pdf', bbox_inches='tight')
-plt.show()
-
-r_tot = np.append(r1,r2)
-fig, ax = plt.subplots()
-ax.hist(r_tot, bins=b, density=True, label=r'$\rho(r)$')
-ax.plot(r, rho_unscreened, '--', color='black', label=r'$\rho_{unscreened}(r)$', linewidth=2)
-ax.plot(r, rho_optimized , '--', color='red', label=r'$\rho_{screened}(r)$', linewidth=2)
-
-ax.set_title(r'Benchmarking $\rho(r)$ sampling, $\alpha=0.1$', fontsize='16')
-ax.set_xlabel(r'$r$ [$a_0$]', fontsize='16')
-ax.set_ylabel(r'$\rho$', fontsize='16')
-
-ax.grid()
-ax.legend(fontsize='16', loc='upper right')
-
-plt.savefig('../T1/figure/probability_benchmark_r.pdf', format='pdf', bbox_inches='tight')
-plt.show()
 
 #%%
 
-r1 = np.arange(-1,1)
-r2 = np.arange(-1,1)
-phi = np.exp(-2*r1)*np.exp(-2*r2)
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-# Make data.
-r1 = np.arange(0, 1, 0.01)
-r2 = np.arange(0, 1, 0.01)
-R1, R2 = np.meshgrid(r1, r2)
-theta = 60*np.pi/180
-R12 = np.sqrt(R1**2 + R2**2 - 2*R1*R2*np.cos(theta))
-Z = np.exp(-2*R1)*np.exp(-2*R2)*np.exp(R12/(2*(1+0.9*R12)))
-surf = ax.plot_surface(R1, R2, Z)
-
-plt.show()
+fft = np.fft.fft(signal)
 
 #%%
 
-E = (1/N)*np.sum(El)
+spectrum   = np.genfromtxt('../T1/spectrum.csv', delimiter=',')
+freq       = np.genfromtxt('../T1/freq.csv', delimiter=',')
 
+fig, ax = plt.subplots()
+ax.plot(freq, spectrum)
 
+ax.set_xlim([0,5])
 
