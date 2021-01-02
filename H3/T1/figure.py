569412f462862b3c9c4ed4b5b0fe6bd18675760d
erikadamstrandberg <erikadamstrandberg@gmail.com>
2021-01-02 12:09:33 +0100

    Visualization of initial gauss

--- H3/T1/figure.py
+++ H3/T1/figure.py
@@ -27,44 +27,65 @@ def wave_package_position(x, x0, p0, d):
     hbar_si = 1.054571817e-34
     hbar_asup = hbar_si*1e34/1.602
     
-    prefactor = (1/(np.pi*d**2))**(1/4)
+    prefactor = np.sqrt(np.sqrt(np.pi)*d)
     envelope  = np.exp(-(x - x0)**2/(2*d**2))
     plane_wave = np.exp(1j*p0*(x - x0)/hbar_asup)
-    return prefactor*envelope*plane_wave
+    return (1/prefactor)*envelope*plane_wave
+
+def wave_package_position_evo(x, x0, t, p0, d, m):
+    hbar_si = 1.054571817e-34
+    hbar_asup = hbar_si*1e34/1.602
+    
+    prefactor = np.sqrt(np.sqrt(np.pi)*(d+1j*hbar_asup*t/(m*d)))
+    envelope  = np.exp(-(x - x0 - p0*t/m)**2/(2*d**2*(1+1j*hbar_asup*t/(m*d**2))))
+    plane_wave = np.exp(1j*p0*(x - x0 - p0*t/(2*m))/hbar_asup)
+    return (1/prefactor)*envelope*plane_wave
 
 d   = 0.5               # Width of our hydrogen atom
 m_h = 1*m_asup_u        # Mass of our hydrogen atom
 
 p0 = np.sqrt(0.2*m_h)   # Initial momentum of our hydrogen atom
-x0 = 0.4                # Initial position of our hydrogen atom
+x0 = 0.0                # Initial position of our hydrogen atom
 
 dx      = 0.01
-x_start = -100
-x_stop  = 100
+x_start = -10
+x_stop  = 10
 x = np.arange(x_start, x_stop, dx)
 Nx = len(x)
 
 phi_x = wave_package_position(x, x0, p0, d)
 n_x = np.abs(phi_x)**2
 
+phi_x_1 = wave_package_position_evo(x, x0, 0.0, p0, d, m_h)
+n_x_1 = np.abs(phi_x_1)**2
+
 fig, ax = plt.subplots()
 ax.plot(x, n_x)
+ax.plot(x, n_x_1)
 
-#%%
+plt.savefig('../T1/initial_gauss_position.pdf', format='pdf', bbox_inches='tight')
+plt.show()
 
-p = np.fft.fftshift(np.fft.fftfreq(Nx, d=dx))
+#%% Fourier transforming to momentum space
 
-phi_p = np.fft.fftshift(np.fft.fft(phi_x))
-n_p = np.abs(phi_p)**2
- 
+dp = 1/(Nx*dx)                              # Delta momentum
+p_nyquist = 1/(2*dx)                        # Nyquivst momentum
+p = np.arange(-p_nyquist,p_nyquist,dp)      # Generating momentum "freq"
+
+phi_p = np.fft.fftshift(np.fft.fft(phi_x))  # Shifting values from transform
+n_p = np.abs(phi_p)**2                      # Probability density
 
 x = np.array([0,0])
 y = np.array([0,17500])
+
 fig, ax = plt.subplots()
 ax.plot(p, n_p)
 ax.plot(x,y)
 
 ax.set_xlim([-1,1])
+
+plt.savefig('../T1/initial_gauss_momentum.pdf', format='pdf', bbox_inches='tight')
+plt.show()
 
 
 #%%
