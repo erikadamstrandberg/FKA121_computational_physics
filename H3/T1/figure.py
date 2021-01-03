Fixed normalization momentum domain. Do not understand it

H3/T1/figure.py
@@ -3,7 +3,6 @@
 # Standard includes
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
 
 #%% Rescaling into ASU_prim units for H3b
 
@@ -17,13 +16,10 @@ m_asup = E_asup*T_asup**2/L_asup**2 # Mass unit in ASU_prim
 m_asup_u = m_asup/u                 # Mass unti in ASU_prim per u
 
 hbar_si = 1.054571817e-34           # hbar in SI
-hbar_asup = hbar_si*1e34/1.602      # hbar in ASU_prim. eV*fs
+hbar_asup = hbar_si/(E_asup*T_asup) # hbar in ASU_prim. eV*fs
 
 print(f"Mass in asup: {m_asup_u} u")
 print(f"h_bar in asup: {hbar_asup} eV*fs")
-
-hbar_si = 1.054571817e-34
-hbar_asup = hbar_si*1e34/1.602
 
 #%% Plotting the Gaussian wave packet
 
@@ -31,32 +27,37 @@ def wave_package_position(x, x0, p0, d, hbar_asup):
     prefactor = np.sqrt(np.sqrt(np.pi)*d)
     envelope  = np.exp(-(x - x0)**2/(2*d**2))
     plane_wave = np.exp(1j*p0*(x - x0)/hbar_asup)
-    return (1/prefactor)*envelope#*plane_wave
+    return (1/prefactor)*envelope*plane_wave
 
 def wave_package_position_evo(x, x0, t, p0, d, m, hbar_asup):   
     prefactor = np.sqrt(np.sqrt(np.pi)*(d+1j*hbar_asup*t/(m*d)))
-    envelope  = np.exp(-(x - x0 - p0*t/m)**2/(2*d**2*(1+1j*hbar_asup*t/(m*d**2))))
+    envelope  = np.exp(-(x - x0 - p0*t/m)**2/(2*d**2*(1 + 1j*hbar_asup*t/(m*d**2))))
     plane_wave = np.exp(1j*p0*(x - x0 - p0*t/(2*m))/hbar_asup)
     return (1/prefactor)*envelope*plane_wave
 
-def width_position_anal(t, d, m, hbar_asup):
+def x_width_anal(t, d, m, hbar_asup):
     prefactor = d/np.sqrt(2)
     time_factor = np.sqrt(1 + hbar_asup**2*t**2/(m**2*d**4))
     return prefactor*time_factor
 
-def width_position(n, dx):
+def p_width_anal(t, d, m, hbar_asup):
+    prefactor = d/np.sqrt(2)
+    time_factor = np.sqrt(1 + hbar_asup**2*t**2/(m**2*d**4))
+    return prefactor*time_factor
+
+def std_from_FWHM(n, dx):
     max_value = np.max(n)
-    width_limit = max_value/np.exp(1)
-    return np.sum(n > width_limit)*dx
+    width_limit = max_value/2
+    return np.sum(n > width_limit)*dx/(2*np.sqrt(2*np.log(2)))
 
 d   = 0.5               # Width of our hydrogen atom
 m_h = 1*m_asup_u        # Mass of our hydrogen atom
 
-p0 = np.sqrt(0.2*m_h)   # Initial momentum of our hydrogen atom
-x0 = 0.0                # Initial position of our hydrogen atom
+p0 = np.sqrt(0.2*m_h)+2  # Initial momentum of our hydrogen atom
+x0 = 0.0               # Initial position of our hydrogen atom
 
-dx      = 0.001
-x_start = -50.0
+dx      = 0.0001
+x_start = -50
 x_stop  = 50.0
 x = np.arange(x_start, x_stop, dx)
 Nx = len(x)
@@ -64,53 +65,66 @@ Nx = len(x)
 phi_x = wave_package_position(x, x0, p0, d, hbar_asup)
 n_x = np.abs(phi_x)**2
 
-print(width_position(n_x, dx))
-print(width_position_anal(0.0, d, m_h, hbar_asup))
-
-
-t1 = 0.01
+t1 = 0.03
 phi_x_1 = wave_package_position_evo(x, x0, t1, p0, d, m_h, hbar_asup)
 n_x_1 = np.abs(phi_x_1)**2
 
+print(std_from_FWHM(n_x_1, dx))
+print(x_width_anal(t1, d, m_h, hbar_asup))
+
+print(np.sum(n_x_1)*dx)
+
 fig, ax = plt.subplots()
 ax.plot(x, n_x)
-#ax.plot(x, n_x_1)
+ax.plot(x, n_x_1)
 
 #plt.savefig('../T1/initial_gauss_position.pdf', format='pdf', bbox_inches='tight')
 #plt.show()
 
 #%% Fourier transforming to momentum space
 
-def wave_package_momentum(p, p0, x0, d, hbar_asup):
-    prefactor = np.sqrt(2*np.sqrt(np.pi)*d)
-    envelope  = np.exp(-d**2*(p0 - 2*np.pi*hbar_asup*p)**2/(2*hbar_asup))
+def wave_package_momentum(p, p0, d, hbar_asup):
+    prefactor = np.sqrt(d/(np.sqrt(np.pi)*hbar_asup))
+    envelope  = np.exp(-d**2*(p0 - p)**2/(2*hbar_asup**2))
     return prefactor*envelope
 
+dp        = 2*np.pi*hbar_asup/(Nx*dx)              # Delta momentum
+p_nyquist = 2*np.pi*hbar_asup/(2*dx)               # Nyquivst momentum
+p         = np.arange(-p_nyquist, p_nyquist, dp)   # Generating momentum "freq"
 
-dp = 1/(Nx*dx)                              # Delta momentum
-p_nyquist = 1/(2*dx)                        # Nyquivst momentum
-p = np.arange(-p_nyquist,p_nyquist,dp)      # Generating momentum "freq"
+phi_p = np.fft.fftshift(np.fft.fft(phi_x))         # Shifting values from transform
+n_p   = np.abs(phi_p)**2*dx**2/(2*np.pi*hbar_asup) # Probability density
 
-norm_ortho = np.sqrt(len(phi_x))
-phi_p = np.fft.fftshift(np.fft.fft(phi_x))/norm_ortho  # Shifting values from transform
-n_p = np.abs(phi_p)**2                      # Probability density
+print(np.sum(n_p)*dp)
 
-phi_p_anal = wave_package_momentum(p, p0, x0, d, hbar_asup)
+phi_p_anal = wave_package_momentum(p, p0, d, hbar_asup)
 n_p_anal = np.abs(phi_p_anal)**2
 
-x = np.array([0,0])
-y = np.array([0,17500])
+print(np.sum(n_p_anal)*dp)
+
+print(std_from_FWHM(n_p, dp))
+print(hbar_asup/(np.sqrt(2)*d))
+
+plot_p0_x = np.array([p0, p0])
+plot_p0_y = np.array([0, np.max(n_p)])
 
 fig, ax = plt.subplots()
 ax.plot(p, n_p)
 ax.plot(p, n_p_anal)
-#ax.plot(x,y)
+#ax.plot(plot_p0_x, plot_p0_y)
 
-ax.set_xlim([-2,2])
+ax.set_xlim([-7,7])
 
-plt.savefig('../T1/initial_gauss_momentum.pdf', format='pdf', bbox_inches='tight')
-plt.show()
+#plt.savefig('../T1/initial_gauss_momentum.pdf', format='pdf', bbox_inches='tight')
+#plt.show()
 
+
+phi_p_1 = np.fft.fftshift(np.fft.fft(phi_x_1)) # Shifting values from transform
+n_p_1   = np.abs(phi_p_1)**2*dx**2/(2*np.pi*hbar_asup)       
+
+ax.plot(p, n_p_1)
+
+ax.set_xlim([-7,7])
 
 #%%
 